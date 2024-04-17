cbf6c7d3e1c0c0a45b4f5ff90ed65a62483ca1b7
MathiasHouwen <mathias.houwen@student.uhasselt.be>
2024-04-17 15:56:31 +0200

    added navbar svelete component

---
+++ src/routes/navbar.svelte
@@ -0,0 +1,12 @@
+<nav>
+    <li>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <ul><a href="">Home</a></ul>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <ul><a href="">Contact</a></ul>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <ul><a href="">Profile</a></ul>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <ul><a href="">Login in</a></ul>
+    </li>
+</nav>
